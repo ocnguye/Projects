b0db063623f7827837b2cc66d83c972687e6717b
joonhoswe <joonhoswe@gmail.com>
2024-02-24 16:46:41 -0500

    building skeleton of login page

---
+++ frontend/src/Login.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Login: React.FC = () => {
+    // Inline styles for the container
+    const containerStyle = {
+        backgroundColor: "#DFF0D8", 
+        display: "flex", 
+        justifyContent: "center", // Center horizontally
+        alignItems: "center", // Center vertically
+        height: "100vh",
+        width: "100vw",
+        margin: 0 // Ensure there's no margin around the edges
+    };
+
+    // Inline styles for the heading
+    const headingStyle = {
+        color: "#94CF6B"
+    };
+
+    return (
+        <div style={containerStyle}>
+            <h1 style={headingStyle}>Angel Trading Co.</h1>
+        </div>
+    );
+};
+
+export default Login;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+//import App from './App'
+import Login from './Login'
 import './index.css'
 import { ClerkProvider } from '@clerk/clerk-react'
 
@@ -21,7 +22,8 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
     <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
-      <App />
+      {/*<App /> */}
+      <Login />
     </ClerkProvider>
     </QueryClientProvider>
   </React.StrictMode>,
