35f19c18ec94eca0b62ee9b93566a2bbb8a33f8f
joonhoswe <joonhoswe@gmail.com>
2024-02-24 18:20:46 -0500

    tailwindcss updates

--- frontend/components/Login.tsx
+++ frontend/components/Login.tsx
@@ -1,26 +1,14 @@
 import React from 'react';
 
 const Login: React.FC = () => {
-    // Inline styles for the container
-    const containerStyle = {
-        backgroundColor: "#DFF0D8", 
-        display: "flex", 
-        justifyContent: "center", // Center horizontally
-        alignItems: "center", // Center vertically
-        height: "100vh",
-        width: "100vw",
-        margin: 0 // Ensure there's no margin around the edges
-    };
-
-    // Inline styles for the heading
-    const headingStyle = {
-        color: "#94CF6B"
-    };
 
     return (
-        <div style={containerStyle}>
-            <h1 style={headingStyle}>Angel Trading Co.</h1>
-            <p className="text-red-900"> test</p>
+        <div className = "bg-green-200 flex flex-col items-center h-screen w-screen">
+            <div className = "bg-green-300 h-3/4 w-4/5">
+                <h1 >Angel Trading Co.</h1>
+                <p className="text-red-900"> test</p>
+            </div>
+            
         </div>
         
     );
