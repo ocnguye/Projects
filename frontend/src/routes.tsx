c6d84f2c61268b635b91545ef98973ace777c138
zenaid <zejzhu@gmail.com>
2024-02-29 16:49:22 -0500

    reroute About to new about page

--- frontend/src/routes.tsx
+++ frontend/src/routes.tsx
@@ -3,12 +3,13 @@ import App from './App'
 import Home from '../components/homepage/home'
 import Searched from '../components/homepage/searched';
 import Profile from '../components/profile/profile';
+import About from '../components/about/about';
 
 export const router = createBrowserRouter([
     {path: '/', element: <App />, children: [
         {path: '', element: <Home />},
         {path: '/search', element: <Searched />},
-        {path: 'about', element: <div>About</div>},
+        {path: 'about', element: <About />},
         {path: 'help', element: <div>Help</div>},
         {path: 'contactus', element: <div>Contact Us</div>},
         {path: 'profile', element: <Profile />},
