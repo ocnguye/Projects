d5ef07be589bc1df47a68e6d3f6989075fa149a8
joonhoswe <joonhoswe@gmail.com>
2024-03-01 15:32:41 -0500

    added pw confirmation, extra smiski img

--- frontend/components/Login.tsx
+++ frontend/components/Login.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useState } from 'react';
 import image1 from './image1.png';
-// import image2 from './image2.png';
+import image2 from './image2.png';
 import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
 import { useSignIn, useSignUp } from '@clerk/clerk-react';
 //import { useNavigate } from "react-router-dom";
@@ -12,6 +12,7 @@ const Login: React.FC = () => {
     const [userName, setUserName] = useState("");
     const [userEmail, setUserEmail] = useState("");
     const [userPassword, setUserPassword] = useState("");
+    const [userConfirmPassword, setUserConfirmPassword] = useState("");
 
     const { signIn } = useSignIn();
     const { isLoaded, signUp } = useSignUp();
@@ -49,38 +50,46 @@ const Login: React.FC = () => {
                         <p className="text-gray-800 text-l p-4 text-center"> The premier Smiski trading platform. </p>
                     </div>
                 </div>
-
+                
                 <div className="bg-yellow-350 flex justify-center items-center relative h-full w-2/3 shadow-2xl">
                     
                     <div className="absolute top-4 right-32"> 
                         <p className = "text-s"> Already have an account? </p>
                     </div>
-                    <div className="absolute top-4 right-4 "> 
+
+                    <div className="absolute top-4 right-2"> 
                         <button className="flex justify-center items-center text-s hover:scale-110 h-6 w-24 px-4 py-1 bg-green-350 text-black rounded-lg transition duration-300 ease-in-out hover:bg-green-450 outline outline-green-450 outline-3" onClick={goToLogin}>
                         Sign in
                         </button>
                     </div>
-                    {/* <div className = "absolute bottom-1 right-2">
-                        <img src = { image2 } alt = "img" className = "h-1/5 w-1/5 pl-4"></img>
-                    </div> */}
+
+                    <div className = "absolute bottom-0 right-4">
+                        <img src = { image2 } alt = "img" className = "h-96 w-64"></img>
+                    </div>
+
+
 
                     <div className="flex flex-col h-3/5 pr-40">
                         <h1 className="text-4xl font-bold"> Welcome to </h1>
                         <h1 className="text-4xl font-bold"> Angel Trading Co.! </h1>
                         <h3 className = "text-lg font-bold pt-6"> Please register your account. </h3>
 
-                        <div className = "flex flex-col justify-between h-2/5 pt-12">
+                        <div className = "flex flex-col justify-between h-2/5 pt-8">
                             <form onSubmit = {onSubmit}>
+
+                                {/* username input field */}
                                 <p className = ""> Username </p>
                                 <label>
                                     <input name = "Username" type = "username" value = {userName} onChange = {(e: React.ChangeEvent<HTMLInputElement>) => setUserName(e.target.value)} className = "bg-gray-200 w-96 rounded-lg"/>
                                 </label>
 
+                                {/* email input field */}
                                 <p className = "pt-4"> Email Address </p>
                                 <label>
                                     <input name = "Email" type = "email" value = {userEmail} onChange = {(e: React.ChangeEvent<HTMLInputElement>) => setUserEmail(e.target.value)} className = "bg-gray-200 w-96 rounded-lg"/>
                                 </label>
 
+                                {/* password input field */}
                                 <p className = "pt-4"> Password </p>
                                 <label className="relative block">
                                     <input name="Password" type={(visibility === false) ? "password" : "text"} value = {userPassword} onChange = {(e: React.ChangeEvent<HTMLInputElement>) => setUserPassword(e.target.value)} className="bg-gray-200 w-96 rounded-lg"/>
@@ -94,9 +103,10 @@ const Login: React.FC = () => {
                                     </div>
                                 </label>
 
+                                {/* confirm password input field - does NOT get passed to Clerk signUp */}
                                 <p className = "pt-4"> Confirm Password </p>
                                 <label className="relative block">
-                                    <input name="ConfirmPassword" type={(visibility === false) ? "password" : "text"}  className="bg-gray-200 w-96 rounded-lg"/>
+                                    <input name="ConfirmPassword" type={(visibility === false) ? "password" : "text"} value = {userConfirmPassword} onChange = {(e: React.ChangeEvent<HTMLInputElement>) => setUserConfirmPassword(e.target.value)}className="bg-gray-200 w-96 rounded-lg"/>
                                     <div onClick={() => setVisibility(!visibility)} className="text-2xl text-gray-700 absolute inset-y-0 right-0 flex items-center pr-3">
                                         {
                                             visibility ?
@@ -107,14 +117,19 @@ const Login: React.FC = () => {
                                     </div>
                                 </label>
 
-                                <p className = "pt-1 text-xs">
-                                    Passwords do not match.
+                                {/* if password field is non-empty, check if passwords match or not */}
+                                <p className = "pt-1 text-xs">  
                                     {
-                                        
-                                    }
+                                        userPassword !== "" ? 
+                                        (userPassword === userConfirmPassword ) ?
+                                        <p className = "text-green-600"> Passwords match! </p>  : <p className = "text-red-500"> ERROR: Passwords do not match. </p> 
+                                        :
+                                        <p className = "text-yellow-350"> btn gap placeholder </p>
+                                    }   
                                 </p>
-                            
-                                <div className="flex justify-center mt-6">
+                                
+                                {/* register button */}
+                                <div className="flex justify-center mt-4">
                                     <input type = "submit" value = "submit" className="hover:scale-110 w-32 bg-green-350 text-black py-2 px-4 rounded-lg transition duration-300 ease-in-out hover:bg-green-450 outline outline-green-450 outline-3" /> 
                                 </div>
                             </form>
