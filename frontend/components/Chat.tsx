36d2e1f4edd95ee0344d5cedce4a3fe4573c482c
joonhoswe <joonhoswe@gmail.com>
2024-03-18 22:09:16 -0400

    frontend updates

--- frontend/components/Chat.tsx
+++ frontend/components/Chat.tsx
@@ -28,13 +28,38 @@ const Chat: React.FC = () => {
     }, [socket])
 
     return (
-        <div className = "text-red-800">
-            <input placeholder='Type a message...' onChange = {(event) => {setMessage(event.target.value)}}/>
-            <button onClick = {sendMessage}> Send </button>
-            <input placeholder='Type a room number...' onChange = {(event) => {setRoom(event.target.value)}}/>
-            <button onClick = {joinRoom}> Join </button>
-            <h1> Message: </h1>
-            {messageReceived}
+        <div className = "bg-yellow-200 w-96 h-screen flex flex-col">
+            {/* Top border with username and buttons */}
+            <div className="flex-grow overflow-auto">
+                <div className="bg-gray-200 w-full flex flex-row items-center text-black justify-between">
+                    <button className="text-m bg-transparent"> &lt; Back </button>
+                    <h2 className="text-xl"> PookiEpstein </h2>
+                    <button className="text-m bg-transparent"> Requests </button>
+                </div>
+
+                <p>{messageReceived}</p>
+            </div>
+
+            {/* Main chat area with messages */}
+            <div className="flex flex-col space-y-2 p-2">
+                <div className="flex items-center">
+                    {/* Temporary room input for testing purposes, will be replaced later */}
+                    <input 
+                        placeholder='Type a room number...' 
+                        onChange={(event) => {setRoom(event.target.value)}} 
+                        className="flex-grow bg-gray-200 p-2 rounded-lg rounded-r-none"
+                    />
+                    <button onClick={joinRoom} className="bg-green-150 p-2 rounded-lg rounded-l-none hover:bg-green-250 active:bg-green-350"> Join </button>
+                </div>
+                <div className="flex items-center">
+                    <input 
+                        placeholder='Type a message...' 
+                        onChange={(event) => {setMessage(event.target.value)}} 
+                        className="flex-grow bg-gray-200 p-2 rounded-lg rounded-r-none"
+                    />
+                    <button onClick={sendMessage} className="bg-green-150 p-2 rounded-lg rounded-l-none hover:bg-green-250 active:bg-green-350 "> Send </button>
+                </div>
+            </div>
         </div>
     );
 };
