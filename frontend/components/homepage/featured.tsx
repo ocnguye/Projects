415e55fd1853c32f68c79ddb1d0695e156ff196a
austinwheel <austinwheeler1112@gmail.com>
2024-03-30 02:01:36 -0400

    styling changes to featured

--- frontend/components/homepage/featured.tsx
+++ frontend/components/homepage/featured.tsx
@@ -36,34 +36,38 @@ const Featured = () => {
         <>
         { !isLoading && !isError ? (
             <a href={data[currentIndex].link}>
-            <div style={{ display: "flex",flexDirection: "column", justifyContent: "center", 
-            alignItems: "center", width: "100%", height: 600, background: "#F2DEDE", 
-            borderRadius: 20, backgroundSize: 'cover',
-            borderWidth: 3, }}
-            className='border-green-800'
+            <div style={{ 
+                width: "100%", 
+                height: 600, 
+                borderRadius: 20, 
+                borderWidth: 3,
+                borderColor: '#79BD44',
+            }}
             >
                 <img src={ cleanImage(data[currentIndex].image) } alt={data[currentIndex].name} 
-                style={{
-                    width: '100%',
-                    height: '100%',
-                    objectFit: 'cover',
-                    borderRadius: '10px', }} />
+                    style={{
+                        height: '100%',
+                        width: '100%',
+                        objectFit: 'cover',
+                        borderRadius: 18,
+                    }}
+                />
             </div>
-                <div style={{ display: "flex", justifyContent: "center", marginTop: 10 }}
-                >
-                    {data.map((_: any, index: any) => (
-                        <div
-                        key={index}
-                        style={{
-                            width: 10,
-                            height: 10,
-                            borderRadius: "50%",
-                            margin: "0 5px",
-                            background: index === currentIndex ? "black" : "gray",
-                        }}
-                        />
-                        ))}
-                </div>
+            <div style={{ display: "flex", justifyContent: "center", marginTop: 10 }}
+            >
+                {data.map((_: any, index: any) => (
+                    <div
+                    key={index}
+                    style={{
+                        width: 10,
+                        height: 10,
+                        borderRadius: "50%",
+                        margin: "0 5px",
+                        background: index === currentIndex ? "black" : "gray",
+                    }}
+                    />
+                    ))}
+            </div>
             </a>
             ) : (
                 <></>
