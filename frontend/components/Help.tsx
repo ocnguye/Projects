f781e956fd2d429f6a45a5a53b3581f852cd9e3a
zenaid <zejzhu@gmail.com>
2024-04-17 15:33:02 -0400

    updated help page

--- frontend/components/Help.tsx
+++ frontend/components/Help.tsx
@@ -6,7 +6,7 @@ const Help: React.FC = () => {
     const [openRows, setOpenRows] = useState<boolean[]>(new Array(6).fill(false));
 
     const toggleRow = (index: number) => {
-        const updatedOpenRows = openRows.map((isOpen, i) => 
+        const updatedOpenRows = openRows.map((isOpen, i) =>
             i === index ? !isOpen : isOpen
         );
         setOpenRows(updatedOpenRows);
@@ -14,8 +14,8 @@ const Help: React.FC = () => {
 
     // Modify each row to include a question and answer
     const rows = [
-        { 
-            title: 'Trading', 
+        {
+            title: 'Trading',
             content: {
                 question: 'Q: How do I make a trade?',
                 answer: 'A: To make a trade, navigate to the Trading page, select the assets you want to trade, and follow the instructions.',
@@ -27,8 +27,8 @@ const Help: React.FC = () => {
                 answer3: 'A: You can use our report feature to report the user and submit evidence / proof including chat logs and pictures of the item you sent. You can also rate the user on their user profile to let other users know. We hold no liability for scams and frauds.'
             }
         },
-        { 
-            title: 'Trade Listing', 
+        {
+            title: 'Trade Listing',
             content: {
                 question: 'Q: How can I list a trade?',
                 answer: 'A: To list a trade, go to the Trade Listing section, fill out the form with your trade details, and submit.',
@@ -38,8 +38,8 @@ const Help: React.FC = () => {
                 answer2: 'A: The listing is automatically removed from your lists! Enjoy the new smiski!'
             }
         },
-        { 
-            title: 'Collections', 
+        {
+            title: 'Collections',
             content: {
                 question: 'Q: How do I log a smiski into my collection?',
                 answer: 'A: To add a new smiski, simply go to the collections tab of your user profile and click the + sign. From there select the smiski you want or search for it and add it.',
@@ -47,8 +47,8 @@ const Help: React.FC = () => {
                 answer1: 'A: Simply click on the collections tab in your user profile and scroll.'
             }
         },
-        { 
-            title: 'Verifications', 
+        {
+            title: 'Verifications',
             content: {
                 question: 'Q: How do I know that my smiski is real?',
                 answer: 'A: While we cannot give away the secrets of our verification process, you can trust with 100% certainty through our algorithm that the smiski is verified. They are verified through a rigous testing process with pictures and smiski experts.',
@@ -56,8 +56,8 @@ const Help: React.FC = () => {
                 answer2: 'A: You can trust it will but if it does not there are two options. You can either resubmit it for verification through a ticket to our support team, or you may have a fraudulent smiski :('
             }
         },
-        { 
-            title: 'Report', 
+        {
+            title: 'Report',
             content: {
                 question: 'Q: How do I report a user?',
                 answer: 'A: If you believe a user has scammed you or sent a fraudulent smiski. Please submit a support ticket in our contact us tab as fast as possible. Be sure to provide chat logs, pictures, and anything else that may help. Unfortunately as we are only facilitating the trade, we cannot guarantee there are no fraudulent users, but we will try our best to resolve the dispute',
@@ -65,8 +65,8 @@ const Help: React.FC = () => {
                 answer1: 'A: You can leave them a bad review, letting others know of their fraudulant behavior, and submit a report ticket. If our team does find the user indicted of criminal behavior, we will ban them from the website.'
             }
         },
-        { 
-            title: 'Contact Us', 
+        {
+            title: 'Contact Us',
             content: {
                 question: 'Q: How do I contact Angel Trading?',
                 answer: 'A: Simply scroll and click on the contact us page task bar and you can submit a request',
@@ -77,30 +77,30 @@ const Help: React.FC = () => {
     ];
 
     return (
-        <div className="bg-green-100 flex flex-col items-center justify-center px-4 py-8 min-h-screen">
-            <div className="bg-green-150 w-3/4 md:w-1/2 lg:w-2/5 h-24 rounded-3xl flex items-center justify-center mb-24 shadow-lg">
-                <p className="text-pink-300 text-3xl font-bold">Hi Pookie, how may we help?</p>
+        <div className="bg-transparent flex flex-col items-center justify-center px-4 py-8 min-h-screen">
+            <div className="bg-white w-3/4 md:w-1/2 lg:w-2/5 h-24 rounded-3xl flex items-center justify-center mb-10 shadow-lg">
+                <h3 className="white" >Hi Pookie, how may we help?</h3>
             </div>
-            <div className="w-full flex flex-col gap-4">
+            <div className="w-3/4 flex flex-col gap-4">
                 {rows.map((row, index) => (
-                    <div key={index} className="bg-green-150 rounded-xl shadow-md overflow-hidden">
-                        <div 
-                            className="cursor-pointer p-4 text-black text-xl font-bold flex justify-between items-center transition duration-300 ease-in-out" 
+                    <div key={index} className="bg-yellow-300 rounded-xl shadow-md overflow-hidden">
+                        <div
+                            className="cursor-pointer p-4 flex justify-between items-center transition duration-300 ease-in-out"
                             onClick={() => toggleRow(index)}
                         >
-                            {row.title}
+                            <h3 className="yellow">{row.title}</h3>
                             <span className="text-lg">{openRows[index] ? '-' : '+'}</span>
                         </div>
                         <div
                             className={`transition-max-height duration-700 ease-in-out ${openRows[index] ? 'max-h-96' : 'max-h-0'} overflow-hidden`}
                         >
-                            <div className="p-4 text-black bg-white">
+                            <div className="p-4 text-green-900 bg-white">
                                 {/* Display the question and answer */}
                                 <p>{row.content.question}</p>
                                 <p className="pt-2">{row.content.answer}</p>
-                                <p className="pt-4">{row.content.question1}</p>
+                                <p className="pt-6">{row.content.question1}</p>
                                 <p className="pt-2">{row.content.answer1}</p>
-                                <p className="pt-4">{row.content.question2}</p>
+                                <p className="pt-6">{row.content.question2}</p>
                                 <p className="pt-2">{row.content.answer2}</p>
                             </div>
                         </div>
