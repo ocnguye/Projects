f8464087343214c03aa66753f37376903d09221d
austinwheel <austinwheeler1112@gmail.com>
2024-04-18 03:45:38 -0400

    make collections page more mobile friendly

--- frontend/components/collection_page/collections.tsx
+++ frontend/components/collection_page/collections.tsx
@@ -53,25 +53,24 @@ const Collection = () => {
         return Object.entries(collectiblesBySeries).map(([series, seriesCollectibles]) => (
             <div key={series} className="flex flex-wrap">
                 <div className="bg-yellow-350 w-full mb-2 pl-3 text-transform: uppercase text-lg">{formatSeries(series)}</div>
-                {seriesCollectibles.map((collection: PCollection) => (
-                    <div key={collection.id} className="w-1/6 p-4 flex flex-col items-center"> 
-                        <img src={getProductImage(collection.image)} style={{ opacity: collection.owned ? 1 : 0.3 }} className="w-50 h-50 object-cover rounded-lg" alt={collection.name} />
-                        {/* <img src={getProductImage(collection.image)} className={`w-50 h-50 object-cover rounded-lg opacity-${collection.owned ? "100" : "30"}`} alt={collection.name} /> */}
-                        <div className="text-center">{collection.name}</div> 
-                    </div>
-                ))}
+                <div className="grid grid-cols-3 md:grid-cols-6">
+                    {seriesCollectibles.map((collection: PCollection) => (
+                        <div key={collection.id} className="w-full p-4 flex flex-col items-center"> 
+                            <img src={getProductImage(collection.image)} style={{ opacity: collection.owned ? 1 : 0.3 }} className="w-50 h-50 object-cover rounded-lg" alt={collection.name} />
+                            <div className="text-center">{collection.name}</div> 
+                        </div>
+                    ))}
+                </div>
             </div>
         ));
     };
 
     return (
         // page ui
-        <div className = "bg-green-150 flex flex-col justify-center h-screen w-screen pl-10 pr-10">
-            <div className = "flex flex-row justify-between w-full">
-                <h1 className = "text-6xl mb-8 text-transform: uppercase"> Your Collection </h1>
-                <div className = "flex">
-                    <div className = "bg-yellow-350 h-7 rounded-lg align-center p-0.5"> Edit Collection </div>
-                </div>
+        <div className = "bg-green-150 flex flex-col justify-center h-screen w-full pl-10 pr-10">
+            <div className = "flex flex-col md:flex-row md:justify-between w-full ">
+                <h1 className = "text-3xl text-transform: mb-5 uppercase"> Your Collection </h1>
+                <div className = "bg-yellow-350 h-7 rounded-lg mb-5 align-center p-0.5 w-fit"> Edit Collection </div>
             </div>
             <div className = "bg-white h-4/5 w-full overflow-y-auto"> 
                 {!isLoading && !isError && data ? sortBySeries(data.collectibles) : <> Loading </>}
